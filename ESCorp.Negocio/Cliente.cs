73c3e2f9b33e341852398cda41ea4c688dac250f
everton.batista <everton.batista@TGSSBC-09016D>
2017-12-22 10:47:08 -0200

    Add project files.

---
+++ ESCorp.Negocio/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ESCorp.Negocio
+{
+    public class Cliente
+    {
+        public int IdCliente { get; private set; }
+
+        private string _sobrenome;
+
+        public string Sobrenome
+        {
+            // Quando queremos obter o valor do campo
+            get
+            {
+                // Lógica qdo necessário
+                return _sobrenome;
+            }
+
+            // Quando queremos atribuir um valor
+            set { _sobrenome = value; }
+        }
+
+        // Propriedados auto implementadas qdo não houver necessidade de lógica
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Ddd { get; set; }
+        public string Telefone { get; set; }
+
+        public string NomeCompleto
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(Nome))                
+                    return Sobrenome;
+                if (string.IsNullOrWhiteSpace(Sobrenome))
+                    return Nome;
+                return Sobrenome + " , " + Nome;
+            }
+        }
+
+        // Ao definir uma propriedade como static ela fica vinculada a sua própria classe e não ao objeto
+        public static int QuantidadeInstancia { get; set; }
+    }
+}
---
+++ ESCorp.Teste/ClienteTeste.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ESCorp.Negocio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ESCorp.Teste
+{
+    [TestClass]
+    public class ClienteTeste
+    {
+        //[Medodo]_[Condicao]_[ResultadoEsperado]
+
+        [TestMethod]
+        public void NomeCompleto_QuandoObterNomeCompleto_DeveRetornarNomeSobrenome()
+        {
+            //Arrange
+            Cliente cliente = new Cliente
+            {
+                Nome = "Everton",
+                Sobrenome = "Batista"
+            };
+
+            string resultadoEsperado = "Batista , Everton";
+            
+            //Act
+            string resultadoAtual = cliente.NomeCompleto;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoAtual);
+
+        }
+
+        [TestMethod]
+        public void NomeCompleto_QuandoPrimeiroNomeVazio_DeveRetornarSomenteSobrenome()
+        {
+            //Arrange
+            Cliente cliente = new Cliente
+            {
+                Nome = "", 
+                Sobrenome = "Batista" // acessando o set da propriedade
+            };
+
+            var nome = cliente.Nome; // acessando o get da propriedade
+
+            string resultadoEsperado = "Batista";
+            //Act
+            string resultadoAtual = cliente.NomeCompleto;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoAtual);
+        }
+
+        [TestMethod]
+        public void NomeCompleto_QuandoSobrenomeVazio_DeveRetornarSomenteSobrenome()
+        {
+            //Arrange
+            Cliente cliente = new Cliente
+            {
+                Nome = "Everton",
+                Sobrenome = ""
+            };
+
+            string resultadoEsperado = "Everton";
+            //Act
+            string resultadoAtual = cliente.NomeCompleto;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, resultadoAtual);
+        }
+    }
+}
